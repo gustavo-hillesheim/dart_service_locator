51175c2655c084bbece978f2daecc5ecdc1b5dc1
gustavo-hillesheim <guhillesheim@gmail.com>
2022-03-12 17:23:32 -0300

    Refactored ServiceLocator

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -2,7 +2,7 @@ import 'package:service_locator/exceptions.dart';
 
 class ServiceLocator {
   final Map<Type, Object?> _instances = {};
-  final Map<Type, Factory?> _factories = {};
+  final Map<Type, _Factory?> _factories = {};
   final List<Type> _servicesBeingLocated = [];
 
   void registerInstance<T extends Object>(T instance) {
@@ -14,14 +14,14 @@ class ServiceLocator {
     bool singleton = true,
     bool lazy = true,
   }) {
-    final factory = Factory(
+    final factory = _Factory(
       factoryFn: factoryFn,
       isSingleton: singleton,
       isLazy: lazy,
     );
     _factories[T] = factory;
-    if (!lazy) {
-      _resolveFactory(factory);
+    if (!lazy && singleton) {
+      _instances[T] = _resolveFactory(factory);
     }
   }
 
@@ -30,46 +30,70 @@ class ServiceLocator {
   }
 
   T locate<T extends Object>() {
+    _checkIsNotBeingLocated<T>();
+    _servicesBeingLocated.add(T);
+    _checkCanBeLocated<T>();
+    final locatedService = (_locateInInstances<T>() ?? _locateInFactories<T>())!;
+    if (locatedService.shouldCache) {
+      _instances[T] = locatedService.instance;
+    }
+    _servicesBeingLocated.remove(T);
+    return locatedService.instance;
+  }
+
+  _LocatedService<T>? _locateInInstances<T extends Object>() {
+    final instance = _instances[T] as T?;
+    if (instance == null) {
+      return null;
+    }
+    return _LocatedService(instance: instance, shouldCache: true);
+  }
+
+  _LocatedService<T>? _locateInFactories<T extends Object>() {
+    final factory = _factories[T] as _Factory<T>?;
+    if (factory == null) {
+      return null;
+    }
+    return _LocatedService(
+      instance: _resolveFactory(factory),
+      shouldCache: factory.isSingleton,
+    );
+  }
+
+  void _checkIsNotBeingLocated<T extends Object>() {
     if (_servicesBeingLocated.contains(T)) {
       throw CircularDependencyException<T>([..._servicesBeingLocated, T]);
     }
-    _servicesBeingLocated.add(T);
+  }
+
+  void _checkCanBeLocated<T extends Object>() {
     if (!canLocate<T>()) {
       throw CouldNotLocateException<T>(_servicesBeingLocated);
     }
-    final T result;
-    if (!_instances.containsKey(T) && _factories.containsKey(T)) {
-      result = _resolveFactory(_factories[T] as Factory<T>);
-    } else {
-      result = _instances[T] as T;
-    }
-    _servicesBeingLocated.remove(T);
-    return result;
   }
 
   bool canLocate<T extends Object>() {
     return _instances.containsKey(T) || _factories.containsKey(T);
   }
 
-  T _resolveFactory<T extends Object>(Factory<T> factory) {
-    final T result;
-    if (factory.isSingleton) {
-      final instance = factory.factoryFn(this);
-      _instances[T] = instance;
-      result = instance;
-    } else {
-      result = factory.factoryFn(this);
-    }
-    return result;
+  T _resolveFactory<T extends Object>(_Factory<T> factory) {
+    return factory.factoryFn(this);
   }
 }
 
-class Factory<T extends Object> {
+class _LocatedService<T> {
+  final T instance;
+  final bool shouldCache;
+
+  _LocatedService({required this.instance, required this.shouldCache});
+}
+
+class _Factory<T extends Object> {
   final FactoryFn<T> factoryFn;
   final bool isSingleton;
   final bool isLazy;
 
-  Factory({
+  _Factory({
     required this.factoryFn,
     required this.isSingleton,
     required this.isLazy,
